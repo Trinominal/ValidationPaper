baea238ace77f0d51b3d530bedc19294f52b5238
Quibus <Vincent.j.wit@gmail.com>
2025-07-10 15:33:34 +0200

    adjustments

--- baldet.py
+++ baldet.py
@@ -12,7 +12,7 @@ def get_data(maskSize: int = 42, ticker: str = 'AAPL',
     # This function should call the get_data function and prepare the input data for the experiment
     # For now, we will just use the get_data function to get the data
     datapoints = yf.download(ticker, start_date, end_date)  # Get the data from the get_data function
-
+    # print('datapoints', datapoints)
     # get the close prices from the data
     close_prices = datapoints['Close'].values  # Get the close prices from the data
 
@@ -22,7 +22,7 @@ def get_data(maskSize: int = 42, ticker: str = 'AAPL',
     # Prepare the input data for the experiment
     input_data = []  # Create an empty array to hold the input data
     reference_data = []
-    for i in range(length - maskSize):
+    for i in range(length - maskSize + 1):
         input_data.append(np.average(close_prices[i:i + maskSize])) # take average add other metrics later
         reference_data.append(close_prices[i:i + maskSize])  # Append the last value of the mask to the reference data
 
@@ -34,18 +34,19 @@ def get_data(maskSize: int = 42, ticker: str = 'AAPL',
     return (input_data, reference_data)
 
 
-def generate_agents(populationSize: int = 5, grounds: int = 1, options: int = 5) -> tuple:
+def generate_agents(populationSize: int = 5, grounds: int = 1, options: int = 5, maskSize: int = 42) -> list:
     """This function generates a collective of agents of size populationSize.
     An agent is implemented as an array of weights ranging between 0 and 100"""
     collective = []
-    for i in range(populationSize):
-        ws = np.random.rand((options, options, grounds, 2))  # Randomly initialize the weight system
+    for _ in range(populationSize):
+        ws = np.random.rand(options, options, grounds+maskSize, 2)  # Randomly initialize the weight system
         ws = np.clip(ws, 0, 1)  # Ensure weights are between 0 and 1
         ws = 100 * ws  # Scale weights to a range of 0 to 100
+        ws = np.round(ws,0)  # Round weights to the nearest integer
         collective.append(ws)
-       
+
     # TODO: implement agent types: risk taking, risk averse, ...
-    return (collective)
+    return collective
 
 
 def simulate_experiment(collective: np.ndarray, data: np.ndarray) -> tuple:
@@ -53,66 +54,85 @@ def simulate_experiment(collective: np.ndarray, data: np.ndarray) -> tuple:
     calculates what each agent would do but also what the collective would do."""
 
     # TODO make sure that we get weights*datapoint for each datapoint
-    colAgent = np.zeros(*collective[0].shape)
+    colAgent = np.zeros((len(data),*collective[0].shape))  # Initialize the collective agent's weights
     weights = []
     for i in range(len(collective)):
-        agent = collective[i]
-        colAgent += agent
-        weights.append(agent*data)
+        agentWeights = np.zeros((len(data[0]),*collective[0].shape))
+        for j in range(len(data)):
+            # For each agent, multiply its weights by the data point
+            print(agentWeights)
+            agentWeights[j,:,:,0,:] = collective[i][:,:,0,:]*data[0][j]
+            print(agentWeights)
+            agentWeights[j,:,:,1:,:] = collective[i][:,:,0:-1,:]*data[1][j]  # Multiply the agent's weights by the data point
+            print(agentWeights)
+            # print(colAgent[j])
+            # print(colAgent[j].shape)/
+            colAgent[j] += agentWeights[j]  # Add the agent's weights to the collective agent's weights
+        weights.append(agentWeights)  # Append the agent's weights to the weights list
 
-    weights.append(colAgent*data)
+    weights.append(colAgent)
+    return
+    # print(weights[0].shape)
 
     # options = ['strong_buy', 'buy', 'hold', 'sell', 'strong_sell']
-    options=5
-    dec = []
-    for x in range(len(collective+1)):
+    options = 5
+    cdec = []
+    for x in range(len(collective)+1):
+        dec = []
         weight_system = weights[x]
-        de = np.ones(len(options)) # Initialize a value array with 1s for each option, 1 means permitted and 0 means not permitted
-        for option1 in range(len(options)):
-            for option2 in range(len(options)):
-                if option1 != option2:
-                    # print('competition test between', i, 'and', j)
-                    # v i becomes not permitted (0) when it gets a -1 somewhere
-                    # v j becomes not permitted (0) when it gets a -1 somewhere 
-                    jwo1 = np.sum(weight_system[option1,option2,:,0])  # Justifying weight of option1 over option2
-                    rwo1 = np.sum(weight_system[option1,option2,:,1])  # Requiring weight of option1 over option2
-                    jwo2 = np.sum(weight_system[option2,option1,:,0])  # Justifying weight of option2 over option1
-                    rwo2 = np.sum(weight_system[option2,option1,:,1])  # Requiring weight of option2 over option1
+        de = np.ones(options) # Initialize a value array with 1s for each option, 1 means permitted and 0 means not permitted
+        print(weight_system.shape)
+        for y in range(len(data)):
+            for option1 in range(options):
+                for option2 in range(options):
+                    if option1 != option2:
+                        # print('competition test between', i, 'and', j)
+                        # v i becomes not permitted (0) when it gets a -1 somewhere
+                        # v j becomes not permitted (0) when it gets a -1 somewhere 
+                        jwo1 = np.sum(weight_system[y,option1,option2,:,0])  # Justifying weight of option1 over option2
+                        rwo1 = np.sum(weight_system[y,option1,option2,:,1])  # Requiring weight of option1 over option2
+                        jwo2 = np.sum(weight_system[y,option2,option1,:,0])  # Justifying weight of option2 over option1
+                        rwo2 = np.sum(weight_system[y,option2,option1,:,1])  # Requiring weight of option2 over option1
 
-                    # print(jwo1, rwo1, jwo2, rwo2)
+                        # print(jwo1, rwo1, jwo2, rwo2)
 
-                    # The values are the sign of the difference between the justifying and requiring weights
-                    v1 = np.sign(jwo1-rwo2)
-                    v2 = np.sign(jwo2-rwo1)
+                        # The values are the sign of the difference between the justifying and requiring weights
+                        v1 = np.sign(jwo1-rwo2)
+                        v2 = np.sign(jwo2-rwo1)
 
-                    if v1 == -1: de[option1] = 0
-                    if v2 == -1: de[option2] = 0 
+                        if v1 == -1: de[option1] = 0
+                        if v2 == -1: de[option2] = 0 
 
-                    dec.append(de)
-   
-    return (dec, weights)
+                        dec.append(de)
+        print('decision', dec)
+        cdec.append(dec)
+
+    return (cdec, weights)
 
 
-def evaluate() -> tuple:
+def evaluate(experiment: list) -> int:
     """ """
 
-    return (0)
+    return 1
 
 
-def plot():
+def plot(evaluation: tuple, experiment: tuple, data: tuple) -> int:
 
 
-    pass
+    return 1
 
 def main() -> int:
     """Main function to handle command line arguments."""
 
-    data = get_data()
-    population = generate_agents()
-    experiment = simulate_experiment(population[0], data[0])
+    data = get_data(maskSize=2, start_date = '2021-07-01', end_date = '2021-07-12')
+    print('data', data[0], data[1])
+    # return 1
+    population = generate_agents(populationSize=1, maskSize=2)
+    experiment = simulate_experiment(population, data)
     evaluation = evaluate(experiment)
+    graph = plot(evaluation, experiment, data)
 
-    return 0
+    return 1
 
 
 if __name__ == '__main__':
